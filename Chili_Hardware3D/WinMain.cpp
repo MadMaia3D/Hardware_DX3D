c9a57198bf3b7b81b2812f44021bf1c520f502c0
MadMaia3D <33674747+MadMaia3D@users.noreply.github.com>
2024-05-24 18:06:30 -0300

    implement: register window class, create window instance and show the window

--- Chili_Hardware3D/WinMain.cpp
+++ Chili_Hardware3D/WinMain.cpp
@@ -1,6 +1,34 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+	const char* const pClassName = "hw3d";
+
+	// Register Window Class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+
+	RegisterClassEx(&wc);
+
+	// Create Window Instance
+	const char* const wndName = "3D Wanna Be";
+	const DWORD wndStyle = WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU;
+
+	HWND hWnd = CreateWindowEx(0, pClassName, wndName, wndStyle, 300, 250, 640, 480, nullptr, nullptr, hInstance, nullptr);	
+
+	// Show The Window
+	ShowWindow(hWnd, SW_SHOW);
+
 	while (true);
 	return 0;
 }
